3bcd634cf416d8c7747239dab17d557c8c944642
istoneyou <nj.leiyou@gmail.com>
2016-04-06 16:29:31 +0800

    init

--- NJUBBS/app/src/main/java/com/stone/njubbs/UI/njuBBSMainActivity.java
+++ NJUBBS/app/src/main/java/com/stone/njubbs/UI/njuBBSMainActivity.java
@@ -1,9 +1,14 @@
 package com.stone.njubbs.UI;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -14,6 +19,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -27,8 +33,17 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 
-public class njuBBSMainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class NJUBBSMainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, TopTenFragment.OnFragmentInteractionListener, BlankFragment.OnFragmentInteractionListener{
+
+    private final int NUM_TAB = 2;
+    private static final int TAB_INDEX_TOP_TEN = 0;
+    private String[] mTabTitles;
+
+
+    private TabLayout mTabLayout = null;
+    private TabsAdapter mTabsAdapter = null;
+    private ViewPager mViewPager = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,22 +52,19 @@ public class njuBBSMainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
         final RequestQueue mQueue = Volley.newRequestQueue(this);
         final StringRequest jsonObjectRequest = new StringRequest("http://bbs.nju.edu.cn/cache/t_top10.js",
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         String s = response.substring(response.indexOf(',') + 1, response.length() - 1);
-                        Log.v("stone", s);
                         try {
                             JSONObject myJsonObject = new JSONObject(s);
-                            Log.v("stone", "tp = " + myJsonObject.getString("tp"));
                             JSONArray mJsonArray = new JSONArray(myJsonObject.getString("tp"));
                             for(int i = 0; i< mJsonArray.length(); i++) {
-                                Log.v("stone", mJsonArray.getJSONObject(i).getString("b"));
                             }
                         } catch (JSONException e) {
-                            Log.v("stone", e.toString());
                         }
 
                     }
@@ -83,9 +95,19 @@ public class njuBBSMainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        TabLayout mTabLayout = (TabLayout) findViewById(R.id.main_tabs);
-        mTabLayout.addTab(mTabLayout.newTab().setText("topTen"));
-        mTabLayout.addTab(mTabLayout.newTab().setText("ss"));
+        initTabs();
+    }
+
+    private void initTabs() {
+        mTabTitles = new String[NUM_TAB];
+        mTabTitles[0] = getString(R.string.tab_title_top_ten);
+        mTabTitles[1] = "others";
+        mTabLayout = (TabLayout) findViewById(R.id.main_tabs);
+        mTabLayout.setTabMode(TabLayout.MODE_FIXED);
+        mViewPager = (ViewPager) findViewById(R.id.main_viewpager);
+        mTabsAdapter = new TabsAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mTabsAdapter);
+        mTabLayout.setupWithViewPager(mViewPager);
     }
 
     @Override
@@ -143,5 +165,44 @@ public class njuBBSMainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public class TabsAdapter extends FragmentPagerAdapter {
+
+        public TabsAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            if (position == 0)
+                return new TopTenFragment();
+            else return new BlankFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_TAB;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            return super.instantiateItem(container, position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return super.getItemId(position);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mTabTitles[position];
+        }
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
---
+++ NJUBBS/app/src/main/java/com/stone/njubbs/Utils/UrlUtils.java
@@ -0,0 +1,8 @@
+package com.stone.njubbs.Utils;
+
+/**
+ * Created by Stone on 2016/4/6.
+ */
+public class UrlUtils {
+    private final String URL_TOP_TEN = "http://bbs.nju.edu.cn/cache/t_top10.js";
+}
